a7731126ddfdd107cd00491a63d817979c47362e
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2025-06-11 09:51:21 -0400

    SPEI download manual instructions

--- 1-Moisture_covariate_overlay.R
+++ 1-Moisture_covariate_overlay.R
@@ -9,6 +9,7 @@ library(sf)
 library(tidyverse)
 library(exactextractr)
 # Download the North Atlantic Oscillation data and save to rds
+# This downloaded .rds file is included in the repo
 # nao <- read.fwf(file = "https://www.cpc.ncep.noaa.gov/products/precip/CWlink/pna/norm.nao.monthly.b5001.current.ascii.table",
 #                   header = FALSE,
 #                 skip = 1,
@@ -17,18 +18,24 @@ library(exactextractr)
 #                 col.names = c("year",month.name)) # accessed on September 18, 2023
 #saveRDS(nao,file = "data/nao.rds")
 
-# saveRDS(pdo,file = "data/pdo.rds")
+
 #load the strata map
 strata_map <- bbsBayes2::load_map("latlong")
 strata_crs <- st_crs(strata_map)
 
-# load moisture data - SPEI values at monthly scale
+
+# Manual download required ------------------------------------------------
+# Download the SPEI data for 03 and 15 months -----------------------------
+# These files are too large for the code repo
 # downloaded from https://doi.org/10.20350/digitalCSIC/15470
+# download spei03.nc to directory data/too_large
+# download spei15.nc to directory data/too_large
 
 ## S.M. Vicente-Serrano, S. Beguería, J.I. López-Moreno. 2010. A Multi-scalar drought index sensitive to global warming: The Standardized Precipitation Evapotranspiration Index – SPEI. Journal of Climate 23: 1696, DOI: 10.1175/2009JCLI2909.1
 
 for(n_months in c("03","15")){
 # These are the means for the preceeding n_months months "spei03" = 3 months, spei15 = previous 15 months.
+# Load the previously downloaded spei data
 moisture_full = terra::rast(paste0("data/too_large/spei",n_months,".nc"))
 
 strata_map = st_transform(strata_map, st_crs(moisture_full))
